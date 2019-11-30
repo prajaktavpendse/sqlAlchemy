332e8a93182779942749637e9b4da3d8ce7de67d
Prajakta <prajaktavpendse@gmail.com>
2019-11-30 14:04:48 -0500

    Task: Created two new customer objects

--- sqlAlchemy.py
+++ sqlAlchemy.py
@@ -6,3 +6,20 @@ from sqlalchemy.orm import sessionmaker, Session
 Session = sessionmaker(bind=engine)
 
 session = Session()
+
+c1 = Customer(first_name='Toby',
+              last_name='Miller',
+              username='tmiller',
+              email='tmiller@example.com',
+              address='1662 Kinney Street',
+              town='Wolfden'
+              )
+
+c2 = Customer(first_name='Scott',
+              last_name='Harvey',
+              username='scottharvey',
+              email='scottharvey@example.com',
+              address='424 Patterson Street',
+              town='Beckinsdale'
+              )
+c1, c2
